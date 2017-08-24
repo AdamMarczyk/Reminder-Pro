c3b03e5d0cfefb4ae068a0775135d469c7020baf
Adam Marczyk <contact@abitofprogramming.com>
2017-08-24 12:03:39 +0200

    add constants and action

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />, document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'),
 );
