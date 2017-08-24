ece0b138c0c84c416868a1a6d0f2be21a583321c
Adam Marczyk <contact@abitofprogramming.com>
2017-08-24 11:49:21 +0200

    add app component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
-  <div>Reminder Pro</div>, document.getElementById('root')
+  <App />, document.getElementById('root'),
 );
